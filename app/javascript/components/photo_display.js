5e000966be6f1dcc22a214a1fc2472e1d54ab200
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-10-21 11:51:11 +0200

    photo display done

--- app/javascript/components/photo_display.js
+++ app/javascript/components/photo_display.js
@@ -2,15 +2,19 @@ const photoDisplay = () => {
   const container = document.querySelector('.photo_display_big');
   const thumbs = document.querySelectorAll('.photo_display_tiny');
 
-  function display(e) {
-    const img = e.target;
-    const imgClone = img.cloneNode(true)
-    container.replaceChild(imgClone, container.firstChild);
-  }
+  if (container) {
+    function display(e) {
+      const img = e.target;
+      const imgClone = img.cloneNode(true)
+      console.log(imgClone)
+      container.replaceChild(imgClone, container.firstChild);
+      console.log(container.firstChild)
+    }
 
-  thumbs.forEach(thumb => {
-    thumb.addEventListener('click', display)
-  });
+    thumbs.forEach(thumb => {
+      thumb.addEventListener('click', display)
+    });
+  }
 };
 
 export { photoDisplay }
