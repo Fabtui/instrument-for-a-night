6e54556fbb982d1b2743ec5dce8ce2f3fe20d39c
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-10-25 12:55:43 +0200

    improve pic display

--- app/javascript/components/photo_display.js
+++ app/javascript/components/photo_display.js
@@ -1,14 +1,16 @@
 const photoDisplay = () => {
   const container = document.querySelector('.photo_display_big');
   const thumbs = document.querySelectorAll('.photo_display_tiny');
+  const bigPicture = document.querySelector('#big-picture')
 
   if (container) {
     function display(e) {
+      while(container.firstChild) {
+        container.removeChild(container.lastChild);
+      }
       const img = e.target;
       const imgClone = img.cloneNode(true)
-      console.log(imgClone)
-      container.replaceChild(imgClone, container.firstChild);
-      console.log(container.firstChild)
+      container.appendChild(imgClone, container.firstChild);
     }
 
     thumbs.forEach(thumb => {
