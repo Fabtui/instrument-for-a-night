034adb1134c510ea26daae07e82d1fc6d615c0b0
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-10-31 18:39:30 +0100

    add front to conversations index

--- app/javascript/components/messages_scroll_down.js
+++ app/javascript/components/messages_scroll_down.js
@@ -1,7 +1,11 @@
 const messagesScrollDown = () => {
   const messagesContainer = document.querySelector('.messages-container')
+  const submitButton = document.querySelector('.message-form-button > input')
   if (messagesContainer) {
     messagesContainer.scrollTo(0, messagesContainer.scrollHeight)
+    submitButton.addEventListener('click', () => {
+      messagesContainer.scrollTo(0, messagesContainer.scrollHeight)
+    })
   }
 }
 
