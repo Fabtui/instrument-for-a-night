e1327177f443f98352057538c2f235098e6ac1e6
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-11-05 23:44:27 +0100

    add dynamic infos display

---
+++ app/javascript/components/calendar_display.js
@@ -0,0 +1,30 @@
+const calendarDisplay = () => {
+  const calendarElements = document.querySelectorAll('.calendar-content');
+  const instrumentDisplay = document.querySelector('#instrument');
+  const dateDisplay = document.querySelector('#date');
+  const priceDisplay = document.querySelector('#price');
+  if (calendarElements) {
+    calendarElements.forEach( calendarElement =>
+    calendarElement.addEventListener('click', (e) => {
+      const rent = e.target.dataset.rent;
+      const id = e.target.dataset.id;
+      const instru = e.target.dataset.name;
+      const price = e.target.dataset.price;
+      const owner = e.target.dataset.owner;
+      const brand = e.target.dataset.brand;
+      const startDate = e.target.dataset.startdate;
+      const endDate = e.target.dataset.enddate;
+      const user = e.target.dataset.user;
+      if (rent === "rent") {
+        instrumentDisplay.textContent = `You've rent the ${owner}'s ${brand} ${instru}`
+      } else {
+        instrumentDisplay.textContent = `${user} have rented your ${brand} ${instru}`
+      }
+      dateDisplay.textContent = `From ${startDate} to ${endDate}`
+      priceDisplay.textContent = `Total price: ${price} €`
+    })
+    )
+  }
+};
+
+export { calendarDisplay }
--- app/javascript/components/calendar_partial.js
+++ app/javascript/components/calendar_partial.js
@@ -4,18 +4,6 @@ const calendarPartial = () => {
     calendarElements.forEach( calendarElement =>
     calendarElement.addEventListener('mouseenter', (e) => {
       const id = e.target.dataset.id
-      const instru = e.target.dataset.name
-      const owner = e.target.dataset.owner
-      const brand = e.target.dataset.brand
-      const startdate = e.target.dataset.startdate
-      const enddate = e.target.dataset.enddate
-      const user = e.target.dataset.user
-      console.log(user)
-      console.log(brand)
-      console.log(owner)
-      console.log(instru)
-      console.log(startdate)
-      console.log(enddate)
       const partials = document.querySelectorAll(`#calendar-partial-${id}`)
       partials.forEach( partial => {
         partial.classList.remove("hidden")
