9c3820c87e71a2788248dcd6ba0c736a70ca5830
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-11-07 17:14:36 +0100

    add a script to hide/show the map on index

---
+++ app/javascript/components/show_map.js
@@ -0,0 +1,20 @@
+const showMap = () => {
+  const showMapButton = document.getElementById('show-map-button');
+  if (map) {
+    const map = document.querySelector('.map');
+    const result = document.querySelector('.instrument-index-result')
+    const showMapButtonInner = showMapButton.querySelector('i');
+    showMapButton.addEventListener('click', () => {
+      if (showMapButtonInner.classList.contains('fa-map-marked-alt')) {
+        showMapButtonInner.classList = 'fas fa-arrow-right'
+      } else {
+        showMapButtonInner.classList = 'fas fa-map-marked-alt'
+      }
+      map.classList.toggle('hidden-map');
+      result.classList.toggle('hidden-map-result');
+    })
+
+}
+}
+
+export { showMap };
